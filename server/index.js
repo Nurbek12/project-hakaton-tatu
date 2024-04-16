8e2bbc89a45d44c4840360f2a2cf150aeee3c9b0
Nurbek12 <rustamovnurbek02@gmail.com>
2024-04-16 19:42:05 +0500

    test 1

--- server/index.js
+++ server/index.js
@@ -3,17 +3,21 @@ import express from 'express'
 import mongoose from 'mongoose'
 import { config } from 'dotenv'
 import routers from './routers.js'
+import url from 'url'
+import { join } from 'path'
 // import { Telegraf } from 'telegraf'
 config()
 
 const app = express()
+const dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 app
   .use(cors({ origin: process.env.ORIGIN }))
   .use(express.json({ limit: '40mb' }))
   .use(express.urlencoded({ limit: '40mb', extended: true }))
+  .use(express.static(join(dirname, '../', 'dist')))
   .use('/api', routers)
-
+  .use('*', (req, res) => res.sendFile(join(dirname,'../','dist','index.html')))
 
 app.listen(process.env.PORT, async () => {
   console.log('server start...')
